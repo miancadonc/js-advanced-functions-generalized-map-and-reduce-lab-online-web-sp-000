06acc41877bdca734d5163b8a245d676efb2d3b9
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-03-27 01:06:13 -0500

    commit tree d3a1e1cd82bd6cc9b2e39cd9ea767c68fbf6f3a1

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(array, func){
+    const newArray = []
+    for(let i = 0; i < array.length; i++){
+        newArray.push(func(array[i]))
+    }
+    return newArray
+}
+
+function reduce(array, func, start=0){
+    let total = (!!start) ? start : array[0]
+    let i = (!!start) ? 0 : 1
+    for(; i < array.length; i++){
+        total = func(array[i], total)
+    }
+    return total
+}
